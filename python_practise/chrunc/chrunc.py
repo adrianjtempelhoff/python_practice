6f05546e40014546e57184208a4880c9d8e0636d
A.J. Tempelhoff <ajtempelhoff@gmail.com>
2021-05-04 20:38:46 +0200

    another way, refactored it further
    using associative array

--- python_practise/chrunc/chrunc.py
+++ python_practise/chrunc/chrunc.py
@@ -1,92 +1,38 @@
 #!/usr/bin/python
 
+# this is in the process of being refactored
+# seems to be working :-)
+
 import sys
-import time
 
-baseword=""
-
-symbols="!@#$%^&*(){}[]|\/.,<>?`~-_=+"
-numbers="0123456789"
-uppers="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowers="abcdefghijklmnopqrstuvwxyz"
-
-tmp=sys.argv[1]
-print tmp
-
-tmp=tmp.replace("^", "(symbol)")
-tmp=tmp.replace("%", "(number)")
-tmp=tmp.replace(",", "(upper)")
-tmp=tmp.replace("@", "(lower)")
-print tmp
-
+addit={"symbols":"!@#$%^&*(){}[]|\/.,<>?`~-_=+","numbers":"0123456789","uppers":"ABCDEFGHIJKLMNOPQRSTUVWXYZ","lowers":"abcdefghijklmnopqrstuvwxyz"}
+tmp=str(sys.argv[1]).replace("^", "(symbol)").replace("%", "(number)").replace(",", "(upper)").replace("@", "(lower)")
+temp2=tmp
 words=[]
-
-temp1=""#"word^%,@"
-temp2=tmp#"word(symbol)(symbol)(number)(upper)(lower)"
-
-#words.append("testing one added")
 words.append(temp2)
+cnt=temp2.count("(symbol)")+temp2.count("(number)")+temp2.count("(upper)")+temp2.count("(lower)")
 
-scount=temp2.count("(symbol)")
-ncount=temp2.count("(number)")
-ucount=temp2.count("(upper)")
-lcount=temp2.count("(lower)")
-
-def do_symbols():
+if cnt > 0:
   for wrd in words:
-    #print wrd
     if wrd.__contains__("(symbol)"):
-      for sym in symbols:
+      for sym in addit["symbols"]:
         n=wrd.replace("(symbol)",sym,1)
         words.append(n)
-
-def do_numbers():
-  for wrd in words:
-    #print wrd
-    if wrd.__contains__("(number)"):
-      for num in numbers:
-        n=wrd.replace("(number)",num,1)
+    elif wrd.__contains__("(number)"):
+      for sym in addit["numbers"]:
+        n=wrd.replace("(number)",sym,1)
         words.append(n)
-
-def do_uppers():
-  for wrd in words:
-    #print wrd
-    if wrd.__contains__("(upper)"):
-      for upp in uppers:
-        n=wrd.replace("(upper)",upp,1)
+    elif wrd.__contains__("(upper)"):
+      for sym in addit["uppers"]:
+        n=wrd.replace("(upper)",sym,1)
         words.append(n)
-
-def do_lowers():
-  for wrd in words:
-    #print wrd
-    if wrd.__contains__("(lower)"):
-      for low in lowers:
-        n=wrd.replace("(lower)",low,1)
+    elif wrd.__contains__("(lower)"):
+      for sym in addit["lowers"]:
+        n=wrd.replace("(lower)",sym,1)
         words.append(n)
-  
-
-if scount > 0:
-  do_symbols()
-
-if ncount > 0:
-  do_numbers()
-
-if ucount > 0:
-  do_uppers()
-
-if lcount > 0:
-  do_lowers()
-#print words
-
-#for jj in range(0,scount):
-#  do_symbols()
-
-#print words
 
 for wrd in words:
   print wrd
-
-# this seems to be working
 
 # usage: python chrunc.py word^%,@
 # ^ symbols
