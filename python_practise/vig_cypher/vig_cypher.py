0c0ab936ab854b38288f26446d79e880a7090dfe
A.J. Tempelhoff <ajtempelhoff@gmail.com>
2021-05-04 21:50:51 +0200

    refactored, input message and rotation
    the code is changed to accept user input for a message and then for the rotation for which to encode the message

--- python_practise/vig_cypher/vig_cypher.py
+++ python_practise/vig_cypher/vig_cypher.py
@@ -1,17 +1,64 @@
 #!/usr/bin/python
 
-print "viginair cypher"
+print "vigenere cypher"
 
 a=["a","b","c","d","e","f","g","h","i","j","k","l","m",
    "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+v=[]
+n=[]
 t=""
 c=0
 for i in a:
-  print i
+  #print i
   for ii in a[c:]:
     t+=ii
+    n+=ii
   for ii in a[:c]:
     t+=ii
-  print t
+    n+=ii
+  #print t
+  v+=[n]
+  n=[]
   t=""
   c+=1
+
+#print v
+
+print v[0]
+
+print v[13]
+
+print v[25]
+
+
+rot=input("what rotation(0-25): ")
+
+print v[rot]
+
+emsg=""
+
+dmsg=raw_input("message: ") #"hack"
+
+print "the message: " + dmsg
+
+for w in dmsg:
+  #print "the letter: " + w
+  f=a.index(w)
+  #print "the index: " + str(f)
+  #print "the encoded letter: " + str(v[rot][f])
+  emsg+=str(v[rot][f])
+  #print "\n"
+
+print emsg
+#emsg=""
+
+#for h in range(0,len(v)):
+#  for w in dmsg:
+#    f=a.index(w)
+#    emsg+=str(v[h][f])
+#  print emsg + " : " + str(h)
+#  emsg=""
+
+
+
+
