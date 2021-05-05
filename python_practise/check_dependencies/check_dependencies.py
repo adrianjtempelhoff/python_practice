7633ddde2744e97e90499d2ddc19ccf9df621091
A.J. Tempelhoff <ajtempelhoff@gmail.com>
2021-05-05 08:07:26 +0200

    added check

---
+++ python_practise/check_dependencies/check_dependencies.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import os
+
+# colored text and background 
+def prRed(skk): print("\033[91m ### {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m   # {}\033[00m" .format(skk)) 
+#def prYellow(skk): print("\033[93m #{}\033[00m" .format(skk)) 
+#def prLightPurple(skk): print("\033[94m #{}\033[00m" .format(skk)) 
+#def prPurple(skk): print("\033[95m #{}\033[00m" .format(skk)) 
+#def prCyan(skk): print("\033[96m #{}\033[00m" .format(skk)) 
+#def prLightGray(skk): print("\033[97m #{}\033[00m" .format(skk)) 
+#def prBlack(skk): print("\033[98m #{}\033[00m" .format(skk)) 
+
+def chck(res):
+  if str(res) != "32512":
+    return "exists"
+  else:
+    return "doesn't exist"
+
+def kchc(dep):
+  res=os.system(str(dep) + " --version > /dev/null 2>&1")
+  #print(dep)
+  return chck(res)
+
+print("This python script checking if the required programs are installed.")
+
+dependent=["ls","lasdfs", "ptrace", "ltrace"]
+#results=[]
+
+for dep in dependent:
+  r=kchc(dep)
+  if r != "exists":
+    prRed(dep + " does not exist")
+  else:
+    prGreen(dep + " exists")
+#  print(dep + " " + kchc(dep))
+  #results[results.len()] = kchc(dep)
+#  prRed("aa")
+
+#j=os.system("ldois --version")
+##print j
+#chck(j)
+
+#j=os.system("ls --version")
+##print j
+#chck(j)
+
+#print os.popen2("lafgads --version", mode='t', bufsize=-1)
